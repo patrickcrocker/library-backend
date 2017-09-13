0f3e89c3b346b0443324364e9c70c1a28465ed07
Patrick Crocker <p.crocker@pivotal-io>
2017-09-13 09:57:48 -0500

    First Commit for backend
    Signed-off-by: Ramkumar Vengadakrishnan <r.vengadakrishnan@pivotal-io>

---
+++ src/test/java/com/pivotal/LibraryBackendApplicationTests.java
@@ -0,0 +1,18 @@
+package com.pivotal;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		assert true;
+	}
+
+}
